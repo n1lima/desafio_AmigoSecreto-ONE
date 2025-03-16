a45c2e57ee7dd06936093d7a4ee4ae90354db1fc
n1lima <nicolima1905@gmail.com>
2025-03-16 16:20:27 -0300

    Adicionar a função "adicionaAmigo()"

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    let inputAmigo  = document.querySelector('#amigo');
+    let nomeAmigo = inputAmigo.value;
+
+    if (nomeAmigo == ''){
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(nomeAmigo);
+        inputAmigo.value = '';
+        inputAmigo.focus();
+    }
+}
+
+function atualizarLista(){
+    
+}
