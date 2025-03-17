6e671c57ae9d367e35a54969161016801ec609d9
n1lima <nicolima1905@gmail.com>
2025-03-17 14:10:46 -0300

    Finalizar o desafio, adicionando a função "sortearAmigo()"

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -8,20 +8,40 @@ function adicionarAmigo(){
 
     if (nomeAmigo == ''){
         alert('Por favor, insira um nome.');
-    } else {
-        amigos.push(nomeAmigo);
-        inputAmigo.value = '';
-        inputAmigo.focus();
-    }
+        return;
+    } 
+
+    amigos.push(nomeAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+
+    atualizarLista();
 }
 
 function atualizarLista(){
     let lista = document.querySelector('#listaAmigos');
     lista.innerHTML = ''; 
 
-    for(let i = 0; i < amigos.length; i++){
-        let elementoLista = document.createElement('li');
-        elementoLista.innerHTML = amigos[i];
-        lista.appendChild(elementoLista);
+     amigos.forEach((amigo) => {
+       let elementoLista = document.createElement('li');
+       elementoLista.textContent = amigo;
+       lista.appendChild(elementoLista);
+     });
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert('Você não adicionou nenhum amigo!');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.querySelector('#resultado');
+    if (resultado){
+        resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+    } else {
+        console.error("Elemento com id='resultado' não encontrado.");
     }
 }
